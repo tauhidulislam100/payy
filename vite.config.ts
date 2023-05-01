a7e45c9a112c025e5d0f043e5ccef4f87918408c
Chukwuka Ezeaka <chukwuka.ezeaka1@outlook.com>
2023-05-01 13:58:49 +0100

    added google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      enableDev: true,
+
+      // Google Analytics (multiple tag can be set with an array)
+      analytics: [
+        {
+          /**
+           * Measurement id
+           */
+          id: "G-ND3DN2D5SS",
+          // disable: true,
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: [
       {
